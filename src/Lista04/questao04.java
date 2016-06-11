6eac7d5391cbf6546a7f8cb0c9d0b2cfe5fed137
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-06-10 21:19:48 -0300

    added questao01 - 05

--- src/Lista04/questao04.java
+++ src/Lista04/questao04.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Lista04;
 
-/**
- *
- * @author a923181
- */
+import java.util.Scanner;
+
+
 public class questao04 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        
+        System.out.println("School grades calculator.");
+        
+         System.out.print("Type 1st grade: ");
+         float g1 = input.nextFloat();
+         
+         System.out.print("Type 2nd grade: ");
+         float g2 = input.nextFloat(); 
+         
+         float avg =  (g1*g2)/2;
+         
+        if (avg >= 9.0f && avg <= 10.0f ){
+             System.out.println("1st grade: " +g1+ " 2nd grade|: " +g2+
+                    "\nAverage: " + avg + 
+                    "\nConcept: A"
+                    +"\nPassed");
+         }
+        else if (avg >= 7.5f && avg <= 8.9f ){
+             System.out.println("1st grade: " +g1+ " 2nd grade|: " +g2+
+                    "\nAverage: " + avg + 
+                    "\nConcept: B" 
+                    +"\nPassed");
+         }
+        else if (avg >= 6.0f && avg <= 7.4f ){
+             System.out.println("1st grade: " +g1+ " 2nd grade|: " +g2+
+                    "\nAverage: " + avg + 
+                    "\nConcept: C"
+                    +"\nPassed");
+         }
+        else if (avg >= 4.0f && avg <= 5.9f ){
+             System.out.println("1st grade: " +g1+ " 2nd grade|: " +g2+
+                    "\nAverage: " + avg + 
+                    "\nConcept: D"
+                    +"\nFailed");
+         }
+        else if (avg >= 0.0f && avg <= 3.9f ){
+             System.out.println("1st grade: " +g1+ " 2nd grade|: " +g2+
+                     "\nAverage: " + avg + 
+                     "\nConcept: E"
+                    +"\nFailed");
+         }
+        else {
+            System.out.println("Something went wrong, try again.");
+        }
+    }
     
 }
